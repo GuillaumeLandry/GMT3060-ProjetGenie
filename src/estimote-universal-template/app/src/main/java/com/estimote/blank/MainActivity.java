936434d918789a772bebdbecbe46bb47ead52140
GuillaumeLandry <guillaumelandry97@hotmail.com>
2022-11-07 23:21:34 -0500

    ajout flask et android local web service

--- src/estimote-universal-template/app/src/main/java/com/estimote/blank/MainActivity.java
+++ src/estimote-universal-template/app/src/main/java/com/estimote/blank/MainActivity.java
@@ -6,10 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView pagenameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,36 @@ public class MainActivity extends AppCompatActivity {
         // if you want to access Estimote Cloud credentials from MyApplication:
         EstimoteCloudCredentials estimoteCloudCredentials =
                 ((MyApplication) getApplication()).estimoteCloudCredentials;
-    }
 
+        pagenameTextView = findViewById(R.id.pagename);
+
+        // creating a client
+        OkHttpClient okHttpClient = new OkHttpClient();
+        // building a request
+        Request request = new Request.Builder().url("http://192.168.0.103:5000/").build();
+
+        // making call asynchronously
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            // called if server is unreachable
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "server down", Toast.LENGTH_SHORT).show();
+                        pagenameTextView.setText("error connecting to the server");
+                    }
+                });
+            }
+
+            @Override
+            // called if we get a
+            // response from the server
+            public void onResponse(
+                    @NotNull Call call,
+                    @NotNull Response response)
+                    throws IOException {pagenameTextView.setText(response.body().string());
+            }
+        });
+    }
 }
